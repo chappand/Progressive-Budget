91470f151f527b5942e1d31e73132bc09485f6d3
Andrew Chappell <chappand@gmail.com>
2021-05-08 18:37:39 -0400

    finished success getAll. Something isn't working. Will find.

--- public/offline.js
+++ public/offline.js
@@ -7,40 +7,48 @@ const request = indexdDB.open("budget", 1);
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
-  };
-  
-  request.onsuccess = function (event) {
+};
+
+request.onsuccess = function (event) {
     db = event.target.result;
-  
+
     if (navigator.onLine) {
         checkDatabase();
     }
-  };
+};
 
-  request.onerror = function (event) {
+request.onerror = function (event) {
     console.log("MISTAKE" + event.target.errorCode);
-  };
+};
 
-  function saveRecord(record) {
+function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.createObjectStore("pending");
-  
-    store.add(record);
-  }  
 
-  function checkDatabase() {
+    store.add(record);
+}
+
+function checkDatabase() {
     const transaction = db.transation("pending", "readwrite");
     const store = transaction.objectStore("pending");
     const getAll = store.getAll;
-  
+
     getAll.onsuccess = function () {
-      if (getAll.result.length > 0) {
-        fetch("/api/transaction/bulk", {
-          method: "POST",
-          body: JSON.stringify(getAll.results),
-          headers: {
-            Accept: "application/json, text/plain, */*",
-            "Content-Type": "application/json",
-          },
-        })
-       
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.results),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            }).then((response) => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+window.addEventListener("online", checkDatabase);
