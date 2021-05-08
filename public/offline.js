5ac888fdc22ea13a57c0f7e799c8e174265957cf
Andrew Chappell <chappand@gmail.com>
2021-05-08 17:57:12 -0400

    On success built, server running.

--- public/offline.js
+++ public/offline.js
@@ -1,3 +1,16 @@
+const { eventNames } = require("../models/Transaction");
+
 let db;
 
 const request = indexdDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
+  
+  request.onsuccess = function (event) {
+    db = event.target.result;
+  
+    if (navigator.onLine) {}
+  };
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
 app.use(logger("dev"));
 
 app.use(compression());
